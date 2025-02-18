ca142ef5606c7a68b2ae3349fbe48f508dbe870d
PaviSrinivasa <pavithra.srinivasa@mpi.nl>
2025-02-18 09:44:56 +0100

    indentation

--- src/main/java/nl/mpi/tla/flat/deposit/fc6/Fc6.java
+++ src/main/java/nl/mpi/tla/flat/deposit/fc6/Fc6.java
@@ -56,122 +56,122 @@ public class Fc6 {
 
 	public static void main(String args[]) throws Exception {
 		
-		XdmNode info = fcrepo(new URI("lat:12345_comic_1_pdf/OBJ/fcr:metadata"));
-                //System.out.print("Info: "+ info.toString());
-		String title = Saxon.xpath2string(info, "normalize-space(//ebucore:filename)",null,NAMESPACES);
-                String mimetype = Saxon.xpath2string(info, "normalize-space(//ebucore:hasMimeType)",null,NAMESPACES);
-                String type = "unknown";
+            XdmNode info = fcrepo(new URI("lat:12345_comic_1_pdf/OBJ/fcr:metadata"));
+            //System.out.print("Info: "+ info.toString());
+            String title = Saxon.xpath2string(info, "normalize-space(//ebucore:filename)",null,NAMESPACES);
+            String mimetype = Saxon.xpath2string(info, "normalize-space(//ebucore:hasMimeType)",null,NAMESPACES);
+            String type = "unknown";
 
-                File mimefile = new File ("./src/main/java/nl/mpi/tla/flat/deposit/fc6/mime-mapping.csv");
+            File mimefile = new File ("./src/main/java/nl/mpi/tla/flat/deposit/fc6/mime-mapping.csv");
 
-                Map<String,List<CsvRecord>> mmap = csvToMap(mimefile);
-                
-                List<CsvRecord> matchingMimetype = mmap.getOrDefault(mimetype, Collections.emptyList()); 
-                System.out.println("matchingMimetype----------> "+matchingMimetype);
-                for( CsvRecord csvR : matchingMimetype){
-                    type = csvR.filetype();
-                    System.out.println("type--> "+type);
-                }
-                
-                try {
-                    String jsonDataString = "{"
-                                            + " \"type\": ["
-                                            + "   {"
-                                            + "      \"target_id\":\"" + type + "\""
-                                            + "   } "
-                                            + " ],"
-                                            + " \"filename\": ["
-                                            + "   {"
-                                            + "       \"value\":\"" + title + "\""
-                                            + "   }"
-                                            + " ],"
-                                            + " \"uri\": ["
-                                            + "   {"
-                                            + "       \"value\": \"fedora://test123/testfile\" "
-                                            + "   }"
-                                            + " ]"
-                                            + "}";                    
-                JSONObject js = new JSONObject();
-                System.out.print(js.toString());
+            Map<String,List<CsvRecord>> mmap = csvToMap(mimefile);
 
-                String endpoint = "https://islandora.traefik.me/entity/file?_format=json";
-
-                HttpClient httpClient = HttpClient.newBuilder().build();
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create(endpoint))
-                        .header("content-type", "application/json")
-                        .POST(BodyPublishers.ofString(jsonDataString))
-                        .header("Authorization", "Basic " + Base64.getEncoder().encodeToString(("admin:admin10"
-                                + "").getBytes()))
-                        .version(HttpClient.Version.HTTP_1_1)
-                        .build();
-
-                HttpResponse response = httpClient.send(request, BodyHandlers.ofString());
-
-                if (response.statusCode()==200 || response.statusCode()==201 ) {
-                    System.out.print(response.headers());
-                    System.out.print(response.body());
-                }
-                else
-                    throw new Exception("Unexpected status["+response.statusCode()+"] while querying! Error msg:");
+            List<CsvRecord> matchingMimetype = mmap.getOrDefault(mimetype, Collections.emptyList()); 
+            System.out.println("matchingMimetype----------> "+matchingMimetype);
+            for( CsvRecord csvR : matchingMimetype){
+                type = csvR.filetype();
+                System.out.println("type--> "+type);
             }
-            catch (Exception e) {
-                System.out.print("Error msg:"+e.getMessage());
-             throw e;   
-            }
-        }
-                
 
-        public static XdmNode fcrepo(URI fid) throws Exception {
-           XdmNode res = null;
-            URI uri = null;
             try {
-                uri = new URI("http://islandora.traefik.me:8081/fcrepo/rest"+"/"+fid.toString());
-            } catch (Exception e) {
-                throw new Exception(e);   
-            }
+                String jsonDataString = "{"
+                                        + " \"type\": ["
+                                        + "   {"
+                                        + "      \"target_id\":\"" + type + "\""
+                                        + "   } "
+                                        + " ],"
+                                        + " \"filename\": ["
+                                        + "   {"
+                                        + "       \"value\":\"" + title + "\""
+                                        + "   }"
+                                        + " ],"
+                                        + " \"uri\": ["
+                                        + "   {"
+                                        + "       \"value\": \"fedora://test123/testfile\" "
+                                        + "   }"
+                                        + " ]"
+                                        + "}";                    
+            JSONObject js = new JSONObject();
+            System.out.print(js.toString());
 
-            try (FcrepoResponse response = new GetBuilder(uri, fedoraClient)
-                .accept("application/rdf+xml")
-                .perform()) {
-                    res = Saxon.buildDocument(new StreamSource(response.getBody()));
-            } catch (Exception e) {
-                 throw e;   
+            String endpoint = "https://islandora.traefik.me/entity/file?_format=json";
+
+            HttpClient httpClient = HttpClient.newBuilder().build();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endpoint))
+                    .header("content-type", "application/json")
+                    .POST(BodyPublishers.ofString(jsonDataString))
+                    .header("Authorization", "Basic " + Base64.getEncoder().encodeToString(("admin:admin10"
+                            + "").getBytes()))
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .build();
+
+            HttpResponse response = httpClient.send(request, BodyHandlers.ofString());
+
+            if (response.statusCode()==200 || response.statusCode()==201 ) {
+                System.out.print(response.headers());
+                System.out.print(response.body());
             }
-            return res;
+            else
+                throw new Exception("Unexpected status["+response.statusCode()+"] while querying! Error msg:");
+        }
+        catch (Exception e) {
+            System.out.print("Error msg:"+e.getMessage());
+         throw e;   
+        }
+    }
+                
+
+    public static XdmNode fcrepo(URI fid) throws Exception {
+       XdmNode res = null;
+        URI uri = null;
+        try {
+            uri = new URI("http://islandora.traefik.me:8081/fcrepo/rest"+"/"+fid.toString());
+        } catch (Exception e) {
+            throw new Exception(e);   
         }
 
-        public static Map<String, List<CsvRecord>> csvToMap(File mimefile) {
-            Map<String,List<CsvRecord>> map = new HashMap<>();
-            try (BufferedReader br = new BufferedReader(new FileReader(mimefile))){
-                String line;
-                while ((line = br.readLine()) != null){
-                    String[] parts = line.split(";");
+        try (FcrepoResponse response = new GetBuilder(uri, fedoraClient)
+            .accept("application/rdf+xml")
+            .perform()) {
+                res = Saxon.buildDocument(new StreamSource(response.getBody()));
+        } catch (Exception e) {
+             throw e;   
+        }
+        return res;
+    }
 
-                    if(parts.length >= 7){
-                        String mimetype = parts[0].trim();
-                        String filetype = parts[1].trim();
-                        String islandora_model = parts[2].trim(); 
-                        String mediatype = parts[3].trim();
-                        String media_use = parts[4].trim();
-                        String media_bundle_target_id = parts[5].trim();
-                        String relation = parts[6].trim();
+    public static Map<String, List<CsvRecord>> csvToMap(File mimefile) {
+        Map<String,List<CsvRecord>> map = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(mimefile))){
+            String line;
+            while ((line = br.readLine()) != null){
+                String[] parts = line.split(";");
 
-                        CsvRecord csvRecord = new CsvRecord(filetype, islandora_model, mediatype, media_use, media_bundle_target_id, relation);
-                        map.putIfAbsent(mimetype, new ArrayList<>());
-                        map.get(mimetype).add(csvRecord);
-                    }
+                if(parts.length >= 7){
+                    String mimetype = parts[0].trim();
+                    String filetype = parts[1].trim();
+                    String islandora_model = parts[2].trim(); 
+                    String mediatype = parts[3].trim();
+                    String media_use = parts[4].trim();
+                    String media_bundle_target_id = parts[5].trim();
+                    String relation = parts[6].trim();
+
+                    CsvRecord csvRecord = new CsvRecord(filetype, islandora_model, mediatype, media_use, media_bundle_target_id, relation);
+                    map.putIfAbsent(mimetype, new ArrayList<>());
+                    map.get(mimetype).add(csvRecord);
                 }
+            }
 
-                map.forEach((mimetype,items) -> {
-                   System.out.println (mimetype + " -> " + items );
-                });
-            }
-            catch(IOException e){
-                System.out.println("Something went wrong");
-            }
-            return map;
+            map.forEach((mimetype,items) -> {
+               System.out.println (mimetype + " -> " + items );
+            });
         }
+        catch(IOException e){
+            System.out.println("Something went wrong");
+        }
+        return map;
+    }
 }
                 
           
